cf94c5b8ef7f3b2a3c544fbc637faef1b73abac1
ruzen42 <malledeferlive@gmail.com>
2025-07-25 23:29:58 +0500

    fix all bugs, in Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,10 @@
-module Lib where
+module Lib (convert) where
 
 import System.FilePath (takeExtension)
 import Data.Char (toLower)
 import Codec.Picture
-import Codec.Picture.Types
 
-data ImageFormat = Jpeg | Png | Svg | Gif
+data ImageFormat = Jpeg | Png | Gif
   deriving (Show, Eq)
 
 getFormat :: FilePath -> Maybe ImageFormat 
@@ -22,14 +21,17 @@ readImageFile filePath = do
   case getFormat filePath of 
     Just Jpeg -> Codec.Picture.readJpeg filePath 
     Just Png  -> Codec.Picture.readPng filePath 
+    Just Gif  -> Codec.Picture.readGif filePath 
     Nothing   -> return $ Left $ "This format is not supported" ++ filePath
 
-writeImageFile :: FilePath -> DynamicImage -> ImageFormat -> IO (Either String ())
+writeImageFile :: FilePath -> DynamicImage -> ImageFormat -> IO ()
 writeImageFile outputPath img format = do
-  result <- case format of
+  case format of
     Jpeg -> Codec.Picture.saveJpgImage 80 outputPath img 
     Png  -> Codec.Picture.savePngImage outputPath img
-  return result
+    Gif  -> do 
+      let _ = Codec.Picture.saveGifImage outputPath img 
+      return ()
 
 convert :: FilePath -> FilePath -> IO ()
 convert inputPath outputPath = do
@@ -43,9 +45,7 @@ convert inputPath outputPath = do
       case eimg of
         Left err -> putStrLn $ "Error reading source image: " ++ err
         Right img -> do
-          eWriteResult <- writeImageFile outputPath img targetFormat
-          case eWriteResult of
-            Left err -> putStrLn $ "Error writing image: " ++ err
-            Right _  -> putStrLn $ "Successfully converted: " ++ inputPath ++ " -> " ++ outputPath
+          writeImageFile outputPath img targetFormat
+          putStrLn $ "Successfully converted: " ++ inputPath ++ " -> " ++ outputPath
 
 
